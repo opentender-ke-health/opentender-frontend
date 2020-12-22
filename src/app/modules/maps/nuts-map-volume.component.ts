ef0c0609eee4c4ccda5c4ac907c91041fe8c8d8f
Alexey Shevchenko <os@consulting.mk>
2020-12-22 19:31:15 +0200

    fix map

--- src/app/modules/maps/nuts-map-volume.component.ts
+++ src/app/modules/maps/nuts-map-volume.component.ts
@@ -17,8 +17,8 @@ import * as Config from '../../../../config.dist.js';
 		</div>
 		<div class="graph-toolbar-container">
 			<div class="graph-toolbar graph-toolbar-right">
-				<button class="tool-button" [ngClass]="{down:map_level==1}" (click)="setLevel(1)" i18n>Counties</button>
-				<button class="tool-button" [ngClass]="{down:map_level==2}" (click)="setLevel(2)" i18n>Parishes</button>
+				<button class="tool-button" [ngClass]="{down:map_level==1}" (click)="setLevel(1)" i18n>Provinces</button>
+				<button class="tool-button" [ngClass]="{down:map_level==2}" (click)="setLevel(2)" i18n>Counties</button>
 			</div>
 		</div>
 		<graph nutsmap [data]="data" [level]="map_level" [title]="title" ></graph>`
